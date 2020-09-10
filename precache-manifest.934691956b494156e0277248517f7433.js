5c6f7a46a12c594cba12d3c7fa3e6d92d63a5e94
Andrew Weiss <wandrew8@gmail.com>
2020-09-10 09:26:13 -0700

    Updates

---
+++ precache-manifest.934691956b494156e0277248517f7433.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7f2d6642d56c34200048abc41198fe9",
+    "url": "/design-systems/index.html"
+  },
+  {
+    "revision": "e6e5051e2c7fb0ac0da3",
+    "url": "/design-systems/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "42bab88cd8f5e0d98dc6",
+    "url": "/design-systems/static/js/2.128e8bff.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/design-systems/static/js/2.128e8bff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6e5051e2c7fb0ac0da3",
+    "url": "/design-systems/static/js/main.b3af00d2.chunk.js"
+  },
+  {
+    "revision": "a0691af8afd79c0d059e",
+    "url": "/design-systems/static/js/runtime-main.0a8ff3e3.js"
+  }
+]);
