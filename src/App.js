c707932f8e57609c45f4c9cac4bcc23edc6037f2
Andrew Weiss <wandrew8@gmail.com>
2020-09-10 09:12:25 -0700

    adds styled components css in js dependancy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import styled from 'styled-components';
 import './App.css';
+
+const Container = styled.div`
+  text-align: center;
+  h1 {
+    font-size: 4rem;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <h1>Design Systems</h1>
+    </Container>
   );
 }
 
